45dfe46e40328c149117ed0b8414b1168a5a3293
MathiasRasmussen <47177932+MathiasRasmussen@users.noreply.github.com>
2019-02-01 13:00:00 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -96,7 +96,8 @@ function data(txt){
 }	
 
 function sendData() { // send data to Arduino
-	 var data = stringToBytes(messageInput.value);
+	// var data = stringToBytes(messageInput.value);
+	var data = stringToBytes ("1");
 	ble.writeWithoutResponse(ConnDeviceId, blue.serviceUUID, blue.txCharacteristic, data, onSend, onError);
 }
 	
